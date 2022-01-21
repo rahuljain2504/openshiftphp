775c1dc3ac5ea3f814b1d95f14d90b38c077adf6
rahuljain2504 <98149460+rahuljain2504@users.noreply.github.com>
2022-01-21 13:39:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,8 @@
 	</head>
 
 <body> 
-	<h1>PHP 2022  Test</h1>
-		<p><b>  Mayank modi DANSKEIT BANK  21st jan 2022 </b></p>
+	<h1>PHP 2022  POD</h1>
+		<p><b>  Rahul Jain attending OpenShift Day 4 on 21st jan 2022 </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
