9d5d75dbbf20228d069708c6750e7ec738247e7e
rahuljain2504 <98149460+rahuljain2504@users.noreply.github.com>
2022-01-21 13:52:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 
 <body> 
 	<h1>PHP 2022  POD</h1>
-		<p><b>  Rahul Jain attending OpenShift Day 4 on 21st jan 2022. Is it Day 4 already ? </b></p>
+		<p><b>  Rahul Jain attending OpenShift Day 4 on 21st jan 2022. Is it Day 4 already ?? </b></p>
 		<?php echo "The Current Date and yes las  Time is: <br />";
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information VERSION 2 </h2> 
